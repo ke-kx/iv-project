67155eab0d6a06a3b9f1f395f173013bd20e948d
Nils Kunze <kunze.nils@gmail.com>
2016-12-01 16:40:17 +0000

    Displayed data and selector content is now updated with respect to the selected filters

--- load_data.js
+++ load_data.js
@@ -1,40 +1,30 @@
-/*
- plan
- 0. display data in some way
- 1. howto: super simple multi select
- 2. create them for all the necessary filters (country, age group, ...),
-      but automatically! (select agegroup or something), there is probably javascript ability for that
- 3. apply them to data with on_update function, ensure they all are applied correctly.
- (think about smart order for performance )
-*/
-
 // --- global variables
 var full_dataset, filtered_dataset;
 var unique_columns, filtered_unique_columns;
 
 var columns = [
-    { head: 'Gender', cl: 'title', filter: [],
+    { head: 'Gender', cl: 'gender', filter: [],
       html: function(r) { return r.Gender; } },
-    { head: 'Riskgroup', cl: 'center', filter: [],
+    { head: 'Riskgroup', cl: 'riskgroup', filter: [],
       html: function(r) { return r.Risk; } },
-    { head: 'Infection Age', cl: 'num', filter: [],
+    { head: 'Infection Age', cl: 'age_at_infection', filter: [],
       html: function(r) { return r.age_at_infection; } },
-    { head: 'First Positive Test', cl: 'date', filter: [],
+    { head: 'First Positive Test', cl: 'first_positive_test_date', filter: [],
       html: function(r) { return r.first_positive_test_date; } },
-    { head: 'Country of Infection', cl: 'center', filter: [],
+    { head: 'Country of Infection', cl: 'infection', filter: [],
       html: function(r) { return r.infection; } },
-    { head: 'Country of Origin', cl: 'center', filter: [],
+    { head: 'Country of Origin', cl: 'origin', filter: [],
       html: function(r) { return r.origin; } },
-    { head: 'Year of Birth', cl: 'num', filter: [],
+    { head: 'Year of Birth', cl: 'year_of_birth', filter: [],
       html: function(r) { return r.year_of_birth; } },
 ];
 
 // --- entry point
 d3.csv("data/patients_final.csv", function (data) {
-    console.log(data);
     full_dataset = data;
     filtered_dataset = full_dataset;
 
+    // --- Elements that only have to be created once
     //array with all unique values / column, only needs to be generated once
     unique_columns = columns.map(x => get_unique_column(x.html));
     filtered_unique_columns = unique_columns;
@@ -50,29 +40,47 @@ d3.csv("data/patients_final.csv", function (data) {
         .append('th')
         .attr('class', function(x) { return x.cl; })
         .text(function(x) { return x.head; });
-
     table.append('tbody');
 
-    //generate content
+    // create one selection box for each column
+    var select_boxes = d3.select('.selectors')
+        .selectAll('select')
+        .data(columns).enter()
+        .append('select')
+        .attr('class', 'selectpicker').attr('multiple', 'multiple')
+        .attr('id', x => x.cl)
+        .on('change', x => update_filters());
+
+    //--- generate content
     update_select_boxes();
     generate_table();
 });
 
 // main update function, reads currently selected filters and applies them before updating the display
 function update_filters() {
-    console.log('new filter arrays:')
     //get currently selected (or unselected values for all ids in columns.head)
     for (var i = 0; i < columns.length; i++) {
-        columns[i].filter = $('#'+columns[i].head).val();
-        //if (!columns[i].filter)
+        columns[i].filter = $('#'+columns[i].cl).val();
     }
 
     // filter
     filtered_dataset = full_dataset.filter(filter_function);
-    filtered_unique_columns = unique_columns.filter(filter_function);
+    filtered_unique_columns = unique_columns.map(column_filter);
 
     update_select_boxes();
     generate_table();
+}
+
+//
+function column_filter(column, i) {
+    // no filter selected for this column -> only get possible values
+    if (columns[i].filter.length == 0) {
+        //TODO: find out which is quicker! // drop completely because it's too slow for full dataset?
+        //column.filter(x => filtered_dataset.map(y => columns[i].html(y)).includes(x))
+        return get_unique_column(columns[i].html)
+    }
+    // some filter selected -> display complete column
+    return column;
 }
 
 // returns true if each attribute is eather in the filter list or the filter list is empty
@@ -93,20 +101,21 @@ function filter_function(x) {
 
 function update_select_boxes(){
 
-    // create one selection box for each column
-    var select_boxes = d3.select('.selectors')
-        .selectAll('select')
-        .data(columns).enter()
-        .append('select')
-        .attr('class', 'selectpicker').attr('multiple', 'multiple')
-        .attr('id', x => x.head)
-        .on('change', x => update_filters());
+    // select all select boxes options and connect them with new data
+    var select_contents = d3.select('.selectors').selectAll('select').data(columns)
+        .selectAll('option')
+        .data((row, i) => filtered_unique_columns[i])
 
-    // fill them with the appropiate filtered values
-    select_boxes.selectAll('option')
-        .data((row, i) => filtered_unique_columns[i]).enter()
+    // update existing elements
+    select_contents.text(function(x) {return x});
+
+    // create new elements as needed
+    select_contents.enter()
         .append('option')
         .text(function(x) {return x});
+
+    // remove old elements
+    select_contents.exit().remove();
 
     // necessary call to update the select box display
     $('.selectpicker').selectpicker('refresh');
@@ -119,7 +128,7 @@ function get_unique_column(attr_function) {
     var lookup = {};
     var result = [];
 
-    for (var item, i = 0; item = full_dataset[i++];) {
+    for (var item, i = 0; item = filtered_dataset[i++];) {
       var name = attr_function(item);
 
       if (!(name in lookup)) {
@@ -135,53 +144,34 @@ function generate_table(){
 
     // DATA JOIN
     // Join new data with old elements, if any.
-    var table = d3.select('table').select('tbody');
+    var table = d3.select('table').select('tbody')
+        .selectAll('tr').data(filtered_dataset);
 
-    // create table body
-    table.selectAll('tr')
-        .data(filtered_dataset).enter()
-        .append('tr')
-        .selectAll('td')
-        .data(function(row, i) {
-            return columns.map(function(c) {
-                // compute cell values for this specific row
-                var cell = {};
-                d3.keys(c).forEach(function(k) {
-                    cell[k] = typeof c[k] == 'function' ? c[k](row,i) : c[k];
-                });
-                return cell;
-            });
-        }).enter()
-        .append('td')
+    // Update existing rows
+    table.selectAll('td').data(get_row_function)
         .html(function(x) { return x.html; })
         .attr('class', function(x) { return x.cl; });
 
-    // Update existing elements
-    table.transition()
-        .duration(0);
+    // Add new rows
+    table.enter()
+        .append('tr')
+        .selectAll('td')
+        .data(get_row_function).enter()
+        .append('td')
+        .html(function(x) { return x.html; })
+        .attr('class', function(x) { return x.cl; });
 
     // Remove old elements as needed.
     table.exit().remove();
 }
 
-/*
-d3.select("body")
-  .selectAll("p")
-  .data([4, 8, 15, 16, 23, 42])
-  .enter().append("p")
-    .text(function(d) { return "I’m number " + d + "!"; });
-
-// Update…
-var p = d3.select("body")
-  .selectAll("p")
-  .data([4, 8, 15, 16, 23, 42])
-    .text(function(d) { return d; });
-
-// Enter…
-p.enter().append("p")
-    .text(function(d) { return d; });
-
-// Exit…
-p.exit().remove();
-*/
-
+function get_row_function(row, i) {
+    return columns.map(function(c) {
+        // compute cell values for this specific row
+        var cell = {};
+        d3.keys(c).forEach(function(k) {
+            cell[k] = typeof c[k] == 'function' ? c[k](row,i) : c[k];
+        });
+        return cell;
+    });
+}
